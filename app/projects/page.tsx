61fe4cb28e0ededc20577923b1cbd07d5d3b514c
Homero Arellano <homeroarellano272@gmail.com>
2026-02-13 22:34:35 -0800

    skeleton for projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,18 @@
+import NavBar from "@/components/NavBar";
+
+const projects = [
+  {
+    name: "name",
+    summary: "summary",
+    stack: "Next.js, DaisyUI, Tailwind",
+  },
+];
+
+export default function ProjectsPage() {
+  return (
+    <div className="mx-auto w-full max-w-screen flex flex-col items-center justify-center">
+      <NavBar />
+      <h1 className="text-lg md:text-4xl font-bold"> Under Construction </h1>
+    </div>
+  );
+}
