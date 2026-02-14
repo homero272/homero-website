22559b0087bbba447592a4859737cc90430f3c9e
Homero Arellano <homeroarellano272@gmail.com>
2026-02-13 22:34:07 -0800

    skeleton for contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "@/components/NavBar";
+
+export default function ContactPage() {
+  return (
+    <div className="mx-auto w-full max-w-screen flex flex-col items-center justify-center">
+      <NavBar />
+      <h1 className="text-lg md:text-4xl font-bold"> Under Construction </h1>
+    </div>
+  );
+}
