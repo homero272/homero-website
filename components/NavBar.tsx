bbd119e747194263984ccb1280d6b3934a2f6b3c
Homero Arellano <homeroarellano272@gmail.com>
2026-02-13 22:45:25 -0800

    changed tailwind css for text sizes

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,32 +1,32 @@
 "use client";
 export default function NavBar() {
   return (
-    <div className="navbar bg-black shadow-sm">
+    <div className="navbar bg-black shadow-sm z-20">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl" href="/home">
-          Homero's Website
+        <a className="btn btn-ghost text-xs md:text-2xl" href="/home">
+          Homero Arellano
         </a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a href="/">Landing Page</a>
+            <a className="text-xs md:text-lg" href="/">Landing Page</a>
           </li>
           <li>
-            <a href="/home">Home</a>
+            <a className="text-xs md:text-lg" href="/home">Home</a>
           </li>
           <li>
             <details>
-              <summary>Pages</summary>
+              <summary className="text-xs md:text-lg">Pages</summary>
               <ul className="bg-base-100 rounded-t-none p-2">
                 <li>
-                  <a href="/about">About</a>
+                  <a className="text-xs md:text-lg" href="/about">About</a>
                 </li>
                 <li>
-                  <a href="/projects">Projects</a>
+                  <a className="text-xs md:text-lg" href="/projects">Projects</a>
                 </li>
                 <li>
-                  <a href="/contact">Contact</a>
+                  <a className="text-xs md:text-lg" href="/contact">Contact</a>
                 </li>
               </ul>
             </details>
