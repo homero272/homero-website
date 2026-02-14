d95505e1290aa2243a89758880488f5085205be1
Homero Arellano <homeroarellano272@gmail.com>
2026-02-13 21:20:21 -0800

    Update home page layout and interactions

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,77 @@
-import Image from "next/image";
+"use client";
 
+import Link from "next/link";
+import LiquidEther from "@/components/LiquidEther";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const [loading, setloading] = useState(false);
+  const router = useRouter();
+  const continueHandler = (btn: React.MouseEvent<HTMLButtonElement>) => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+      router.push("/about");
+    }, 2000);
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="relative overflow-hidden">
+      <div className="fixed inset-0 z-0 pointer-events-none">
+        <div style={{ width: "100%", height: "100%", position: "relative" }}>
+          <LiquidEther
+            colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+            mouseForce={20}
+            cursorSize={100}
+            isViscous
+            viscous={30}
+            iterationsViscous={32}
+            iterationsPoisson={32}
+            resolution={0.5}
+            isBounce={false}
+            autoDemo
+            autoSpeed={0.5}
+            autoIntensity={2.2}
+            takeoverDuration={0.25}
+            autoResumeDelay={3000}
+            autoRampDuration={0.6}
+            color0="#5227FF"
+            color1="#FF9FFC"
+            color2="#B19EEF"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+      <main className="relative z-10 mx-auto flex w-full max-w-6xl flex-col items-center justify-start gap-8 px-6 pt-10 md:pt-14">
+        <h1 className="whitespace-nowrap text-2xl md:whitespace-nowrap md:text-4xl font-semibold text-white text-center">
+          Welcome to Homero's {""}
+          <span className="inline-flex items-center gap-2 text-rotate">
+            <span>
+              <span className="bg-teal-400 text-teal-800 px-2">Website</span>
+              <span className="bg-red-400 text-red-800 px-2">Portfolio</span>
+              <span className="bg-blue-400 text-blue-800 px-2">Story</span>
+            </span>
+          </span>
+        </h1>
+        <div className="mockup-code typewriter w-full max-w-3xl min-h-[25vh] py-8 text-base leading-relaxed md:text-xl mt-50">
+          <pre data-prefix="$">
+            <code>npm i homero-website</code>
+          </pre>
+          <pre data-prefix=">" className="text-warning">
+            <code>installing...</code>
+          </pre>
+          <pre data-prefix=">" className="text-success">
+            <code>Done!</code>
+          </pre>
         </div>
+        {!loading ? (
+          <button className="btn btn-success" onClick={continueHandler}>
+            Success
+          </button>
+        ) : (
+          <button className="btn">
+            <span className="loading loading-spinner"></span>
+            getting things ready...
+          </button>
+        )}
       </main>
     </div>
   );
