6441547d7aa2d63d75814d3250427ca0123e55dd
Homero Arellano <homeroarellano272@gmail.com>
2026-02-13 21:46:47 -0800

    changed navbar bg color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function NavBar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-black shadow-sm">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl" href="/home">
           Homero's Website
