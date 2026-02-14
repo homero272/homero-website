a2eeb250814aac5bf7dd90bd2cf310cd98df98ad
Homero Arellano <homeroarellano272@gmail.com>
2026-02-13 22:46:09 -0800

    chaned btn names and icons

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
         </div>
         {!loading ? (
           <button className="btn btn-success" onClick={continueHandler}>
-            Continue
+            npm run build
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -73,14 +73,28 @@ export default function Home() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z"
               />
             </svg>
           </button>
         ) : (
           <button className="btn">
             <span className="loading loading-spinner"></span>
-            getting things ready...
+            building...
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+              />
+            </svg>
           </button>
         )}
       </main>
